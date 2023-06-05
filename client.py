ad50614cea93420b1447597e8fdca30a2210c109
“lqqlbb” <3474595102@qq.com>
2023-06-05 08:36:41 +0800

    mac

--- client.py
+++ client.py
@@ -121,9 +121,10 @@ def subscribe(topic:str):
         p.Subscribe(topic)
 if __name__ =="__main__":
     args = sys.argv
+    
     ip,id=getDHCPip()
     print("IP:",ip,"\n","ID:",id,"\n")
-    if (args != "end"):
+    if (args[1] != "end"):
          makeBridge(ip,EDGE_IP)
     time.sleep(2)
     os.system("sudo ip route add default via "+EDGE_IP)
