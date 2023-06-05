034d6ae4456fa17f45fbb89ff896607096faa8b5
“lqqlbb” <3474595102@qq.com>
2023-06-05 08:30:34 +0800

    mac

--- client.py
+++ client.py
@@ -123,7 +123,7 @@ if __name__ =="__main__":
     args = sys.argv
     ip,id=getDHCPip()
     print("IP:",ip,"\n","ID:",id,"\n")
-    if (len(args) != "end"):
+    if (args != "end"):
          makeBridge(ip,EDGE_IP)
     time.sleep(2)
     os.system("sudo ip route add default via "+EDGE_IP)
