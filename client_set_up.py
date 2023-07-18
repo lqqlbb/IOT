1ef0e8ec0f725b458813f13e5aa270a574bf8045
lqqlbb <52438693+lqqlbb@users.noreply.github.com>
2023-07-18 08:00:00 +0800

    Update client_set_up.py

--- client_set_up.py
+++ client_set_up.py
@@ -185,7 +185,7 @@ def check_connection_mqtt():
             # print(mqtt_instance.connected)
             if not mqtt_instance.connected:
             #     # pdb.set_trace()
-                print(mqtt_instance.connected)
+                # print(mqtt_instance.connected)
                 mqtt_instance.Start()
                 time.sleep(3)
         else:
